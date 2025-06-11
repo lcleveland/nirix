89ecd3590bfd820bee4e65aa30d0a1aa1da8c27b
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-06-10 21:56:28 -0500

    test

--- system/desktop_environment/walker.nix
+++ system/desktop_environment/walker.nix
@@ -8,7 +8,7 @@ in
     environment.systemPackages = [
       (inputs.walker.packages.${nix.host_platform}.default.override
         {
-          enable = true;
+          programs.walker.enable = true;
         }
       )
     ];
