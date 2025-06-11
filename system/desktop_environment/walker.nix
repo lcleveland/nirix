5f322b89aa22403b4534243fc9b5b4b2e519b25f
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-06-10 22:24:11 -0500

    do not use system level run as service

--- system/desktop_environment/walker.nix
+++ system/desktop_environment/walker.nix
@@ -8,7 +8,6 @@ in
     environment.systemPackages = [ inputs.walker.packages.${nix.host_platform}.default ];
     programs.walker = {
       enable = desktop_environment.walker.enable;
-      runAsService = desktop_environment.walker.run_as_service;
     };
   };
 }
