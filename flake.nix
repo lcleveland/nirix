c644a7e2f6eae6a36eabcda7a30013ba04e4d382
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-06-11 17:36:45 -0500

    test

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
           /etc/nixos/hardware-configuration.nix
         ];
       make_framework_16 = nixpkgs.lib.nixosSystem {
-        modules = core_modules ++ [ nixos_hardware.nixosModules.framework-16-7040-amd ];
+        modules = lib.mkMerge core_modules ++ [ nixos_hardware.nixosModules.framework-16-7040-amd ];
         specialArgs = { inherit inputs; };
       };
     in
