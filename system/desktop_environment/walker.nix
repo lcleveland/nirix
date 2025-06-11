dca78264da6395779ec2846b37ec57c4a2957caf
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-06-10 21:24:32 -0500

    test

--- system/desktop_environment/walker.nix
+++ system/desktop_environment/walker.nix
@@ -7,10 +7,8 @@ in
   imports = [
     inputs.walker.${nix.host_platform}.default
   ];
-  config = {
-    programs.walker = {
-      enable = desktop_environment.walker.enable;
-      runAsService = desktop_environment.walker.run_as_service;
-    };
+  programs.walker = {
+    enable = desktop_environment.walker.enable;
+    runAsService = desktop_environment.walker.run_as_service;
   };
 }
