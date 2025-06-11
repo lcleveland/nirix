1697b05e47c18f41aea2a4bc850493ab71292d36
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-06-10 21:28:38 -0500

    test

--- system/desktop_environment/walker.nix
+++ system/desktop_environment/walker.nix
@@ -8,9 +8,10 @@ in
     environment.systemPackages = [
       inputs.walker.packages.${pkgs.system}.default
     ];
-    programs.walker = {
-      enable = true;
-      runAsService = desktop_environment.walker.run_as_service;
-    };
+
+  };
+  programs.walker = {
+    enable = true;
+    runAsService = desktop_environment.walker.run_as_service;
   };
 }
