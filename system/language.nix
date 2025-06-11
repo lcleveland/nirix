83a837db43fba9ecc35b3a7f8e9613291d2dfa5d
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-06-10 20:51:45 -0500

    fix settings

--- system/language.nix
+++ system/language.nix
@@ -4,17 +4,19 @@ let
 in
 {
   config = {
-    i18n.defaultLocale = language.locale;
-    extraLocaleSettings = {
-      LC_TIME = language.locale;
-      LC_MONETARY = language.locale;
-      LC_PAPER = language.locale;
-      LC_NAME = language.locale;
-      LC_ADDRESS = language.locale;
-      LC_TELEPHONE = language.locale;
-      LC_MEASUREMENT = language.locale;
-      LC_IDENTIFICATION = language.locale;
-      LC_NUMERIC = language.locale;
+    i18n = {
+      defaultLocale = language.locale;
+      extraLocaleSettings = {
+        LC_TIME = language.locale;
+        LC_MONETARY = language.locale;
+        LC_PAPER = language.locale;
+        LC_NAME = language.locale;
+        LC_ADDRESS = language.locale;
+        LC_TELEPHONE = language.locale;
+        LC_MEASUREMENT = language.locale;
+        LC_IDENTIFICATION = language.locale;
+        LC_NUMERIC = language.locale;
+      };
     };
   };
 }
