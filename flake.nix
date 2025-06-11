1f8767999178f281a002f6dd8b2205a015bb2c44
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-06-11 17:22:17 -0500

    test

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     let
       inputs = core_inputs // nirix_users.inputs;
       import_modules = import ./resources/nix/import_modules.nix;
-      core_modules = import_modules ./system ++
+      core_modules = (import_modules ./system) ++
         nirix_users.nixosModules.default ++
         [
           home_manager.nixosModules.home-manager
