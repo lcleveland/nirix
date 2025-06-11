ee0517f4a1d199d450275e12ce6b40e4abb5bfc3
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-06-10 22:11:26 -0500

    test

--- system/desktop_environment/walker.nix
+++ system/desktop_environment/walker.nix
@@ -4,6 +4,7 @@ let
 in
 {
   config = lib.mkIf desktop_environment.walker.enable {
+    environment.systemPackages = [ inputs.walker.packages.${pkgs.system}.default ];
     programs.walker = {
       enable = desktop_environment.walker.enable;
       runAsService = desktop_environment.walker.run_as_service;
