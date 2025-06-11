1b7a209cf48586212f9a13e210f875a8ea1df512
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-06-10 22:12:14 -0500

    test

--- system/desktop_environment/walker.nix
+++ system/desktop_environment/walker.nix
@@ -1,10 +1,11 @@
-{ config, lib, pkgs, ... }:
+{ config, inputs, lib, pkgs, ... }:
 let
   desktop_environment = config.nirix.system.desktop_environment;
+  nix = config.nirix.system.nix;
 in
 {
   config = lib.mkIf desktop_environment.walker.enable {
-    environment.systemPackages = [ inputs.walker.packages.${pkgs.system}.default ];
+    environment.systemPackages = [ inputs.walker.packages.${nix.host_platform}.default ];
     programs.walker = {
       enable = desktop_environment.walker.enable;
       runAsService = desktop_environment.walker.run_as_service;
