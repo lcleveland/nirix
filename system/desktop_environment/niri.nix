00bbf01bbb4ca820ac6caefcb6a007c3b5c0007d
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-06-11 14:54:35 -0500

    test

--- system/desktop_environment/niri.nix
+++ system/desktop_environment/niri.nix
@@ -11,9 +11,8 @@ in
       {
         settings = {
           binds = {
-            "XF86AudioRaiseVolume".action = spawn "wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1+";
-            "XF86AudioLowerVolume".action = spawn "wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1-";
-            "Mod+T".action = spawn "ghostty";
+            "XF86AudioRaiseVolume".action.spawn = [ "wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1+" ];
+            "XF86AudioLowerVolume".action.spawn = [ "wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1-" ];
           };
           input = {
             keyboard.xkb = {
