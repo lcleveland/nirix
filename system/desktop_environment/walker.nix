67abd548197467720026c20a9c667410eded717c
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-06-10 21:44:59 -0500

    test

--- system/desktop_environment/walker.nix
+++ system/desktop_environment/walker.nix
@@ -6,7 +6,7 @@ in
 {
   config = lib.mkIf desktop_environment.walker.enable {
     environment.systemPackages = [
-      (inputs.walker.${nix.host_platform}.default.override {
+      (inputs.walker.${pkgs.system}.default.override {
         programs.enable = true;
       }
       )
