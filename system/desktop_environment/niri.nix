13fc3a7ff0820224f0cfeb37269246065de544a8
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-06-11 11:03:37 -0500

    niri test

--- system/desktop_environment/niri.nix
+++ system/desktop_environment/niri.nix
@@ -1,8 +1,15 @@
-{ config, lib, pkgs, ... }:
+{ config, home-manager, lib, pkgs, ... }:
 {
   config = {
     programs = {
       niri.enable = true;
     };
+    home-manager.sharedModules = [
+      {
+        programs.niri.settings = {
+          prefer-no-csd = true;
+        };
+      }
+    ];
   };
 }
