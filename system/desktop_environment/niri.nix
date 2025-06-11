229eade5542fc8b26a72b32a848c513bce82a04c
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-06-11 14:56:03 -0500

    test

--- system/desktop_environment/niri.nix
+++ system/desktop_environment/niri.nix
@@ -9,7 +9,7 @@ in
     };
     home-manager.sharedModules = [
       {
-        settings = {
+        niri.settings = {
           binds = {
             "XF86AudioRaiseVolume".action.spawn = [ "wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1+" ];
             "XF86AudioLowerVolume".action.spawn = [ "wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1-" ];
