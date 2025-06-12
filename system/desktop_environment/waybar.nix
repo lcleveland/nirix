95434032c0dff610289c1e71b0fee5aeee3397c6
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-06-12 10:48:17 -0500

    waybar settings

--- system/desktop_environment/waybar.nix
+++ system/desktop_environment/waybar.nix
@@ -8,6 +8,53 @@ in
       {
         programs.waybar = {
           enable = desktop_environment.waybar.enable;
+          settings = {
+            mainBar = {
+              layer = "top";
+              position = "top";
+              height = 24;
+              modules-right = [ "pulseaudio" "network" "cpu" "memory" "battery" "tray" "clock" ];
+              "tray" = {
+                spacing = 10;
+              };
+              "cpu" = {
+                format = "{usage}% ";
+              };
+              "memory" = {
+                format = "{}% ";
+              };
+              "battery" = {
+                bat = "BAT0";
+                adapter = "AC";
+                states = {
+                  good = 85;
+                  warning = 30;
+                  critical = 15;
+                };
+                format = "{capacity}% {icon}";
+                format-icons = [ "" "" "" "" "" ];
+              };
+              "network" = {
+                format-wifi = "{essid} ({signalStrength}%) ";
+                format-ethernet = "{ifname}: {ipaddr}/{cidr} ";
+                format-disconnected = "Disconnected ⚠";
+              };
+              "pulseaudio" = {
+                format = "{volume}% {icon}";
+                format-bluetooth = "{volume}% {icon}";
+                format-muted = "";
+                format-icons = {
+                  headphones = "";
+                  hands-free = "";
+                  headset = "";
+                  phone = "";
+                  portable = "";
+                  car = "";
+                  default = [ "" ];
+                };
+              };
+            };
+          };
         };
       }
     ];
