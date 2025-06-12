9ed1acceeee4f0dd83147abac2c53004df062941
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-06-12 16:38:59 -0500

    test

--- system/desktop_environment/hyprlock.nix
+++ system/desktop_environment/hyprlock.nix
@@ -6,5 +6,12 @@ in
   config = lib.mkIf desktop_environment.hyprlock.enable {
     programs.hyprlock.enable = true;
     security.pam.services.hyprlock = { };
+    home-manager.sharedModules = [
+      {
+        programs.hyprlock = {
+          enable = true;
+        };
+      }
+    ];
   };
 }
