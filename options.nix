4369c88b91f2781ce409fb9cecd9df62c06c6c1b
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-06-13 12:20:53 -0500

    fix

--- options.nix
+++ options.nix
@@ -550,7 +550,7 @@
         };
         time.time_zone = lib.mkOption {
           type = lib.types.str;
-          default = " America/Chicago ";
+          default = "America/Chicago";
           description = ''
                   The
                   time
