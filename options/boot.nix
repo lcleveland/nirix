f71bac56a2d02526c9f22ec6bcf4f040779f5289
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-06-26 10:10:59 -0500

    fix

--- options/boot.nix
+++ options/boot.nix
@@ -9,20 +9,20 @@
           				Whether the EFI variables can be modified by the kernel.
           			'';
       };
-      kernel.kernel_package = lib.mkOption {
-        type = lib.types.attrs;
-        default = pkgs.linuxPackages_latest;
-        description = ''
+    };
+    kernel.kernel_package = lib.mkOption {
+      type = lib.types.attrs;
+      default = pkgs.linuxPackages_latest;
+      description = ''
             					The kernel package to use.
             				'';
-      };
-      systemd.enable = lib.mkOption {
-        type = lib.types.bool;
-        default = true;
-        description = ''
+    };
+    systemd.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = ''
             				Whether to enable the systemd boot service.
             			'';
-      };
     };
   };
 }
