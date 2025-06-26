fb5cae956360692ddcb8407d9b26a85d84bbd5f8
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-06-26 09:38:35 -0500

    test

--- system/desktop_environment/regreet.nix
+++ system/desktop_environment/regreet.nix
@@ -1,5 +1,6 @@
 { config, lib, pkgs, ... }:
 let
+  keyboard = ${config.nirix.config_root}.keyboard;
   regreet = ${config.nirix.config_root}.desktop_environment.regreet;
 in
 {
@@ -7,6 +8,10 @@ in
     programs.regreet = {
       enable = true;
       settings = lib.mkForce ../../resources/regreet/settings.toml;
+    };
+    environment.variables = {
+      XKB_DEFAULT_LAYOUT = keyboard.layout;
+      XKB_DEFAULT_VARIANT = keyboard.variant;
     };
   };
 }
