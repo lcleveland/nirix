fdd3b814d3dc13a9701ed9209ff129e7e363db00
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-06-26 10:27:33 -0500

    fix options

--- system/desktop_environment/niri.nix
+++ system/desktop_environment/niri.nix
@@ -33,7 +33,7 @@ in
             (flag "skip-at-startup")
           ])
           (node "output" [ "edp-1" ] [
-            (leaf "background-color" [ config.nirix.system.theme.colors.compositor_background ])
+            (leaf "background-color" [ config.nirix.system.desktop_environment.theme.colors.compositor_background ])
           ])
           (plain "binds" [
             (plain "XF86AudioRaiseVolume" [ (leaf "spawn" [ "wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1+" ]) ])
