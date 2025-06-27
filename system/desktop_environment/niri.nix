4d443a2dff016f5d38e5cf26fba815f2d84a87a9
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-06-27 11:02:32 -0500

    test

--- system/desktop_environment/niri.nix
+++ system/desktop_environment/niri.nix
@@ -12,10 +12,9 @@ in
       };
     };
     home-manager.sharedModules = [
-      inputs.niri.homeModules.config
       {
         programs.niri.settings = {
-          binds = (with config.lib.niri.actions; {
+          binds = (with lib.niri.actions; {
             "XF86AudioRaiseVolume".action = spawn [ "wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1+" ];
             "XF86AudioLowerVolume".action = spawn [ "wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1-" ];
             "Mod+D".action = lib.mkIf settings.desktop_environment.walker.enable spawn [ "walker" ];
