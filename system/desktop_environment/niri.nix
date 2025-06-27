0ecf1a7cb9382f0342a998174cf7b12c5302f81b
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-06-27 11:15:43 -0500

    test

--- system/desktop_environment/niri.nix
+++ system/desktop_environment/niri.nix
@@ -101,7 +101,6 @@ in
               "Mod+Shift+Minus".action = set-window-height "-10%";
               "Mod+Shift+Equal".action = set-window-height "+10%";
               "Print".action = screenshot;
-              "Ctrl+Print".action = screenshot-screen;
               "Alt+Print".action = screenshot-window;
               "Mod+Shift+E".action = quit;
               "Mod+Shift+P".action = power-off-monitors;
