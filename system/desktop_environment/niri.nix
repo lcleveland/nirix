01f956ea174bbe9cc0d1ba943bbbb67869c942ec
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-06-27 10:19:07 -0500

    test

--- system/desktop_environment/niri.nix
+++ system/desktop_environment/niri.nix
@@ -16,7 +16,7 @@ in
     home-manager.sharedModules = [
       {
         programs.niri.settings = {
-          binds = with config.lib.niri.actions; {
+          binds = lib.mkAfter (with config.lib.niri.actions; {
             "XF86AudioRaiseVolume".action = spawn [ "wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1+" ];
             "XF86AudioLowerVolume".action = spawn [ "wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1-" ];
             "Mod+D".action = lib.mkIf settings.desktop_environment.walker.enable spawn [ "walker" ];
@@ -106,7 +106,7 @@ in
             "Alt+Print".action = screenshot-window;
             "Mod+Shift+E".action = quit;
             "Mod+Shift+P".action = power-off-monitors;
-          };
+          });
           hotkey-overlay = {
             skip-at-startup = true;
           };
