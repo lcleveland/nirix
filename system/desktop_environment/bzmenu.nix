cfbfa7c110ee5d79137172f4c59782725b523836
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-06-28 14:29:20 -0500

    logic

--- system/desktop_environment/bzmenu.nix
+++ system/desktop_environment/bzmenu.nix
@@ -1,10 +1,11 @@
 { config, inputs, lib, pkgs, ... }:
 let
+  bluetooth = config.nirix.system.bluetooth;
   bzmenu = config.nirix.system.desktop_environment.bzmenu;
   nix = config.nirix.system.nix;
 in
 {
-  config = lib.mkIf bzmenu.enable {
+  config = lib.mkIf (bzmenu.enable && bluetooth.enable) {
     environment.systemPackages = [
       inputs.bzmenu.packages.${nix.host_platform}.default
     ];
